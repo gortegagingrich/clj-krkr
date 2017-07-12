f1349661fb687ad70e846fe716dbe08c5473544b
Gabriel <gortegagingrich@gmail.com>
2017-07-11 19:51:13 -0700

    Made changes to strip-line. Count lines works.  Added sample.

--- src/clj_krkr/lines.clj
+++ src/clj_krkr/lines.clj
@@ -1,8 +1,9 @@
 (ns clj-krkr.lines)
 (require '[clojure.string :as str])
+(use 'clojure.java.io)
 
 (def filters
-  [#"\[[^'^,^\]]+\]" #"^@.*" #"^\;.*" #"【.*】"])
+  [#"\[[^,^]+\]" #"^@.*" #"^\;.*" #"【.*】" #"^\*.*"])
 
 (defn strip-line
   ([line] 
@@ -15,5 +16,22 @@
         (str/replace line (first res) "")
         (subvec res 1)))))
 
-(defn count-lines [fPath]
-  (println fPath))
+(defn count-lines
+  ([fPath]
+    (count-lines 
+      (str/split (slurp fPath) #"\n") 0))
+  ([lines n]
+    (if (> (count lines) 0)
+      (if (> (count (strip-line (first lines))) 1)
+        (count-lines (subvec lines 1) (+ n 1))
+        (count-lines (subvec lines 1) n))
+      n)))
+
+(defn print-line [line]
+  (if (> (count line) 1)
+    (println line)))
+
+(defn print-lines [fPath]
+  (with-open [fin (reader fPath :encoding "JISAutoDetect")]
+    (doseq [line (line-seq fin)]
+      (print-line (strip-line line)))))
